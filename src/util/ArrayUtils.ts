c894074d95fecec238e37d1d02fc51e79aa3a51b
c332030 <c332030@gmail.com>
2019-08-05 09:49:18 +0800

    调整数据节点显示方式前备份

---
+++ src/util/ArrayUtils.ts
@@ -0,0 +1,12 @@
+/**
+ * <p>
+ *   Description: ArrayUtils
+ * </p>
+ * @author c332030（袁兴旺）
+ * @version 1.0
+ * @date 2019-8-2 14:22
+ */
+
+export class ArrayUtils {
+
+}
--- src/util/Tools.ts
+++ src/util/Tools.ts
@@ -71,19 +71,19 @@ export class Tools{
   }
 
   /**
-   * 获取 JS 值
+   * 获取值，不允许为空
    * @param obj 对象
    * @param key 值
    * @param defaultValue 默认值
    */
   public static get<E>(
-    obj: any,
-    key: string,
-    defaultValue?: E
+    obj: any
+    ,key: string
+    ,defaultValue: E
   ): E | undefined {
 
     if(!obj || !key) {
-      return defaultValue;
+      return undefined;
     }
 
     const keySpilt = key.split('.');
@@ -96,23 +96,39 @@ export class Tools{
 
       const keyTmp = keySpilt[e];
       if(isEmpty(keyTmp)) {
-        return defaultValue;
+        return undefined;
       }
 
       objTmp = objTmp[keyTmp];
       if(isNull(objTmp)) {
-        return defaultValue;
+        return undefined;
       }
     }
 
     return objTmp;
   }
+
+  /**
+   * 允许为空的 get
+   * @param obj
+   * @param key
+   */
+  public static getNullable<E>(
+    obj: any
+    ,key: string
+  ):E | undefined {
+    return Tools.get(obj, key, undefined);
+  }
 }
 
 export const get = Tools.get;
+export const getNullable = Tools.getNullable;
 
 export const isNull = Tools.isNull;
 export const isEmpty = Tools.isEmpty;
 
 export const notNull = Tools.notNull;
 export const notEmpty = Tools.notEmpty;
+
+export const dealNull = Tools.dealNull;
+export const dealEmpty = Tools.dealEmpty;
